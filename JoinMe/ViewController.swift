623bc1ebd963ab53396b14bd6f63510b264cfb1d
Teddy <teddychen1251@gmail.com>
2018-10-19 23:58:29 -0400

    Initial Commit

---
+++ JoinMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JoinMe
+//
+//  Created by Teddy Chen on 10/19/18.
+//  Copyright © 2018 Teddy Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
