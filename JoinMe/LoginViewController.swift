415b695f0a3bc0e5fd11c8dfb2a68e6fb112cbf8
Tim Tan <timmytan18@gmail.com>
2018-10-20 14:47:26 -0400

    created login view controller

---
+++ JoinMe/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  JoinMe
+//
+//  Created by Tim Tan on 10/20/18.
+//  Copyright © 2018 Teddy Chen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
